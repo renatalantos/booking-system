1244c4ad9070e2839b33ddbb1c9fa51606226801
renatalantos <renatalantos@gmail.com>
2021-10-16 13:06:25 +0000

    Correct line in settings.py

--- reservesmart/settings.py
+++ reservesmart/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'restaurant',
