62499b61482f3ba94979e5536a2f85249022f7ba
renatalantos <renatalantos@gmail.com>
2021-10-16 14:03:21 +0000

    Deleted templates file and created templates folder instead

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Table(models.Model):
+    TABLE_CATEGORY = [
+        ('Inside', 'Inside'),
+        ('Outside', 'Outside'),
+    ]
+    category = models.CharField(max_length=7, choices=TABLE_CATEGORY)
+    table_number = models.IntegerField()
+    seats = models.IntegerField()
+
+    def __str__(self):
+        return f' Table number {self.table_number} {self.category} with {self.seats} seats'
+        
+
