1a2f22c4e5bca5a293a4ca5f1f2cdfc6aaea2541
renatalantos <renatalantos@gmail.com>
2021-10-16 18:01:42 +0000

    Add category Menu to models.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Table, Booking
+from .models import Table, Booking, Menu
 # Register your models here.
 
 
 admin.site.register(Table)
 admin.site.register(Booking)
+admin.site.register(Menu)
--- restaurant/models.py
+++ restaurant/models.py
@@ -5,20 +5,31 @@ from django.contrib.auth.models import User
 class Table(models.Model):
     
     TABLE_CATEGORY = [
-        ('Inside', 'Inside'),
-        ('Outside', 'Outside'),
+        ('For 2', 'Couple'),
+        ('For 4', 'Party of 4'),
+        
     ]
     category = models.CharField(max_length=7, choices=TABLE_CATEGORY)
     table_number = models.IntegerField()
-    seats = models.IntegerField(default=2)
-    
+ 
 
 
     def __str__(self):
-        return f' Table number {self.table_number} {self.category} with {self.seats} seats'
-        
+        return f' Table number {self.table_number} for a {self.category}'
+
+class Menu(models.Model):
+    MENU_CATEGORY = [
+        ('A la carte', 'A la carte'),
+        ('House special', 'Special'),
+        ('Vegetarian', 'Vegetarian'),
+        ('Vegan', 'Vegan'),
+    ]
+    menu_category = models.CharField(max_length=13, choices=MENU_CATEGORY)
+    def __str__(self):
+        return f' {self.menu_category}'
 
 class Booking(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu')
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer')
     table = models.ForeignKey(Table, on_delete=models.CASCADE, related_name='reserved_table')
     start_date = models.DateTimeField()
@@ -27,4 +38,4 @@ class Booking(models.Model):
     number_of_tables = models.IntegerField()
 
     def __str__(self):
-        return f' Customer {self.customer} has booked{self.table} {self.number_of_tables} tables for {self.number_of_customers} customers between {self.start_date} and {self.end_date}'
+        return f' Customer {self.customer} has booked{self.table} {self.number_of_tables} tables for {self.number_of_customers} customers between {self.start_date} and {self.end_date} with menu {self.menu}'
