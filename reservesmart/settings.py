e8e45f75dd281869047c5eff4ff473d28c5e10a9
renatalantos <renatalantos@gmail.com>
2021-10-29 21:19:09 +0000

    Test booking table responsivity in Heroku

--- reservesmart/settings.py
+++ reservesmart/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ["restaurant-reservation-sytem.herokuapp.com", "localhost"]
 
 
